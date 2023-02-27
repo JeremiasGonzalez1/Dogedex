57240d75118bc399d6dceda5556fb191e8998f11
Jeremias Gonzalez <ARX0001313@prosegur.com>
2023-02-27 15:04:21 -0300

    validacion campo sing-up

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,17 @@ q
 Aapp/src/main/java/com/jeredev/dogedex/doglist/DogListViewModel.kt,a\0\a0cc6f253220195280070c7383d6cc816bb1b6df
 m
 =app/src/main/java/com/jeredev/dogedex/api/dto/DogDTOMapper.kt,2\8\28b6fb4aad1e7ff19c539cc4207880a84abd0e15
+e
+5app/src/main/java/com/jeredev/dogedex/MainActivity.kt,d\5\d5477734289899c1dd918cb486883faf5e8abdfa
+n
+>app/src/main/java/com/jeredev/dogedex/doglist/DogRepository.kt,8\4\84e9a8a7df95818e966aed603cf8de7b651ce021
+p
+@app/src/main/java/com/jeredev/dogedex/doglist/DogListActivity.kt,5\e\5e5a820eac1c2e070de14e3b91478705bc80b33c
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+k
+;app/src/main/java/com/jeredev/dogedex/auth/LoginActivity.kt,6\4\644ea076b4b5626b14d071eceae2ef09dec5856d
--- app/src/main/java/com/jeredev/dogedex/auth/SingUpFragment.kt
+++ app/src/main/java/com/jeredev/dogedex/auth/SingUpFragment.kt
@@ -1,6 +1,7 @@
 package com.jeredev.dogedex.auth
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -18,7 +19,51 @@ class SingUpFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSingUpBinding.inflate(inflater, container, false)
+        setSingUpButton()
         return binding.root
     }
 
+    private fun setSingUpButton() {
+        binding.btnSingUp.setOnClickListener {
+            validateFields()
+        }
+    }
+
+    private fun validateFields() {
+        binding.inputUserNew.error = ""
+        binding.inputPasswordNew.error = ""
+        binding.inputRepeatPasswordNew.error = ""
+
+        val email = binding.inputUserNew.editText?.text.toString()
+
+        if (!isValidEmail(email)) {
+            binding.inputUserNew.error = getString(R.string.email_is_not_valid)
+            return
+        }
+
+        val password = binding.inputPasswordNew.editText?.text.toString()
+
+        if (password.isEmpty()) {
+            binding.inputPasswordNew.error = getString(R.string.password_must_not_be_empty)
+            return
+        }
+
+        val passwordRepeat = binding.inputRepeatPasswordNew.editText?.text.toString()
+
+        if (passwordRepeat.isEmpty()) {
+            binding.inputRepeatPasswordNew.error = getString(R.string.password_must_not_be_empty)
+            return
+        }
+
+        if (password != passwordRepeat) {
+            binding.inputPasswordNew.error = getString(R.string.password_do_not_match)
+            return
+        }
+
+        //sing Up
+    }
+
+    private fun isValidEmail(email: String): Boolean {
+        return !email.isNullOrEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
 }
