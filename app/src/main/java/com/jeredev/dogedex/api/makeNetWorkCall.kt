0d46e762c9573ff9722f8f507793d62428e0c246
Jeremias Gonzalez <ARX0001313@prosegur.com>
2023-02-28 11:20:53 -0300

    catch de error de login

--- app/src/main/java/com/jeredev/dogedex/api/makeNetWorkCall.kt
+++ app/src/main/java/com/jeredev/dogedex/api/makeNetWorkCall.kt
@@ -5,9 +5,11 @@ import com.jeredev.dogedex.api.dto.DogDTOMapper
 import com.jeredev.dogedex.api.response.ApiResponseStatus
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import retrofit2.HttpException
 import java.lang.Exception
 import java.net.UnknownHostException
 
+private const val UNAUTHORIZE_ERROR_CODE = 401
 suspend fun <T> makeNetWorkCall(
     call: suspend () -> T
 ): ApiResponseStatus<T> = withContext(Dispatchers.IO) {
@@ -15,12 +17,16 @@ suspend fun <T> makeNetWorkCall(
         ApiResponseStatus.Success(call())
     } catch (e: UnknownHostException) {
         ApiResponseStatus.Error(R.string.unknown_host_exception)
+    } catch (e: HttpException) {
+        val errorMessage =
+            if (e.code() == UNAUTHORIZE_ERROR_CODE) R.string.wrong_user_or_password else R.string.unknown_error
+        ApiResponseStatus.Error(errorMessage)
     } catch (e: Exception) {
-      val errorMessage =  when(e.message){
+        val errorMessage = when (e.message) {
             "sign_up_error" -> R.string.error_sign_up
             "sign_in_error" -> R.string.error_sign_in
             "user_already_exists" -> R.string.error_user_already_exists
-            else->R.string.unknown_error
+            else -> R.string.unknown_error
         }
         ApiResponseStatus.Error(errorMessage)
     }
