1a8e7a312167311d5f04c718fda9e656c82f1361
Jeremias Gonzalez <ARX0001313@prosegur.com>
2023-03-02 14:29:36 -0300

    Se agrega preview de la camara

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,18 +1,6 @@
 
 <
 build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
-@
-app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
-u
-Eapp/src/main/java/com/jeredev/dogedex/api/response/AuthApiResponse.kt,7\9\79dfa1ef53e24e737846ab33171fdb8c5da1abf2
-l
-<app/src/main/java/com/jeredev/dogedex/auth/AuthRepository.kt,c\d\cdf43c737f43cd29111686e6c46d6098aa830299
-k
-;app/src/main/java/com/jeredev/dogedex/auth/AuthViewModel.kt,8\d\8de6ddb5053fc3d52b5f362f6f0c06c4fd433de3
-i
-9app/src/main/java/com/jeredev/dogedex/api/dto/LoginDTO.kt,e\5\e5b2cc4e2b80d920eb6b4c45cbfc68dcd94abef6
-k
-;app/src/main/java/com/jeredev/dogedex/auth/LoginFragment.kt,e\6\e6497f0277091351fee6234b011782ec4e4c58d2
 l
 <app/src/main/java/com/jeredev/dogedex/auth/SingUpFragment.kt,c\3\c33731486b043567f08deb259c29533097e762f5
 ^
@@ -23,3 +11,7 @@ n
 >app/src/main/java/com/jeredev/dogedex/api/dto/UserDTOMapper.kt,1\d\1d41ebc45dbf89a5cc248eda68e4a969aaa3c03d
 h
 8app/src/main/java/com/jeredev/dogedex/api/dto/UserDTO.kt,8\f\8ffc671c41c6cef32bc2edebdb81960c47d82d46
+p
+@app/src/main/java/com/jeredev/dogedex/doglist/DogListActivity.kt,5\e\5e5a820eac1c2e070de14e3b91478705bc80b33c
+n
+>app/src/main/java/com/jeredev/dogedex/doglist/DogRepository.kt,8\4\84e9a8a7df95818e966aed603cf8de7b651ce021
--- app/src/main/java/com/jeredev/dogedex/MainActivity.kt
+++ app/src/main/java/com/jeredev/dogedex/MainActivity.kt
@@ -1,8 +1,18 @@
 package com.jeredev.dogedex
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import com.jeredev.dogedex.api.ApiServiceInterceptor
 import com.jeredev.dogedex.auth.LoginActivity
 import com.jeredev.dogedex.databinding.ActivityMainBinding
@@ -11,9 +21,23 @@ import com.jeredev.dogedex.model.User
 import com.jeredev.dogedex.settings.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                startCamera()
+            } else {
+                Toast.makeText(this, "You need to accept camera permission", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val user = User.getLoggedInUser(this)
@@ -31,6 +55,8 @@ class MainActivity : AppCompatActivity() {
         binding.dogListFab.setOnClickListener {
             openListDogs()
         }
+
+        requestCameraPermissions()
     }
 
     private fun openListDogs() {
@@ -44,5 +70,55 @@ class MainActivity : AppCompatActivity() {
     private fun openLoginActivity() {
         startActivity(Intent(this, LoginActivity::class.java))
         finish()
+    }
+
+    private fun requestCameraPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    startCamera()
+                }
+                shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)
+                -> {
+
+                    AlertDialog.Builder(this).setTitle("Necesitas permisos")
+                        .setMessage("Necesita aceptar los permisos de la camara para que la aplicacion pueda utilizarla")
+                        .setPositiveButton(android.R.string.ok) { _, _ ->
+                            requestPermissionLauncher.launch(
+                                Manifest.permission.CAMERA
+                            )
+                        }.setNegativeButton(android.R.string.cancel) { _, _ ->
+                        }.show()
+
+                }
+                else -> {
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.CAMERA
+                    )
+                }
+            }
+        } else {
+            startCamera()
+        }
+
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            //used to bind  the lifeCycle of cameras to the lifecycle owner
+            val cameraProvider = cameraProviderFuture.get()
+            //preview
+            val preview = Preview.Builder().build()
+            preview.setSurfaceProvider(binding.cameraPreview.surfaceProvider)
+            //select back camera  as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            //bind use cases to camera
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+        }, ContextCompat.getMainExecutor(this))
     }
 }
